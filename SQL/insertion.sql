f103393696a30663a82177694672d9733776e060
malcheikhali <1803528@crosemont.qc.ca>
2022-03-15 13:43:18 -0400

    debut script insertion

---
+++ SQL/insertion.sql
@@ -0,0 +1,9 @@
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (1,"adminadmin01@admin.com","admin123","Admin");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (2,"adminadmin02@admin.com","admin123","Admin");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (3,"EtudiantAli@crosemont.com","vivelaprog","Etudiant");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (4,"EtudiantBroly@crosemont.com","gitgit","Etudiant");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (5,"Dini@prof.com","dini123","Prof");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (6,"toufik@prof.com","toufik123","Prof");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (7,"cgicomp@cgi.com","cgi123","Entreprise");
+insert into compte (IdCompte,Courriel,Password,TypeCompte) values (8,"Ubisoft@ubisoft.com","ubisoft123","Entreprise");
+insert into admin (IdAdmin,Nom,Prenom,Compte_IdCompte) values (1, "jean","michel",1);
