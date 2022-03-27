43f9afe6d437dc3d4114ff426a63e1ead29c267b
Snely Adam Lys <1826841@crosemont.qc.ca>
2022-03-27 02:13:03 -0400

    Page d'inscription codé, manque la validation JS

---
+++ workspace-page-signup/validationInscription.js
@@ -0,0 +1,6 @@
+const NOM = document.getElementById("nom");
+const PRENOM = document.getElementById("prenom");
+const MDP = document.getElementById("mdp");
+const MDP2 = document.getElementById("mdp2");
+const NUMDA = document.getElementById("numDA");
+const COURRIEL = document.getElementById("courriel");
