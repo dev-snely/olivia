55987898d5e2f9539cd1181a93157d361ea94b18
gboisvert <1281921@crosemont.qc.ca>
2022-04-01 10:21:43 -0400

    insertion sql

--- SQL/insertion.sql
+++ SQL/insertion.sql
@@ -1,142 +1,175 @@
-
 use instage;
 INSERT INTO `instage`.`compte`
-(`Courriel`,
+(`IdCompte`,
+`Courriel`,
 `Password`,
 `TypeCompte`)
 VALUES
-("admin@gmail.com",
+(1,
+"Admin@gmail.com",
 "admin",
-"adm");
+"Admin");
 
 INSERT INTO `instage`.`compte`
-(`Courriel`,
+(`IdCompte`,
+`Courriel`,
 `Password`,
 `TypeCompte`)
 VALUES
-("entreprise@gmail.com",
-"123",
-"ent");
+(2,
+"Prof@gmail.com",
+"prof",
+"Prof");
 
-
-INSERT INTO `instage`.`compte` 
-(`Courriel`,
+INSERT INTO `instage`.`compte`
+(`IdCompte`,
+`Courriel`,
 `Password`,
 `TypeCompte`)
 VALUES
-("etudiant@gmail.com",
-"123",
-"etu");
+(3,
+"Entr@gmail.com",
+"entr",
+"Entr");
 
-INSERT INTO `instage`.`compte` 
-(`Courriel`,
+INSERT INTO `instage`.`compte`
+(`IdCompte`,
+`Courriel`,
 `Password`,
 `TypeCompte`)
 VALUES
-("Professeur@gmail.com",
-"123",
-"pro");
+(4,
+"Etu@gmail.com",
+"etu",
+"Etu");
+
 INSERT INTO `instage`.`admin`
-(`Nom`,
+(`IdAdmin`,
+`Nom`,
 `Prenom`,
 `Compte_IdCompte`)
 VALUES
-("Boisvert",
-"Gabriel",
+(1,
+"Smith",
+"Adam",
 1);
+
+
+
+
 INSERT INTO `instage`.`cv`
-(`Resume`,
+(`IdCV`,
+`Resume`,
 `ExperienceTravail`,
 `Education`,
 `Certification`,
 `Competence`,
 `Langue`)
 VALUES
-("un résumé blabla bla",
-"aucune expérience",
-"Finissant diplôme collégial en programmation",
-"aucune",
-"efficace",
-"Français et Anglais"); 
+(1,
+"Blabla resumé",
+"Épicerie Métro",
+"DEC Programmation",
+"Cisco",
+"Travaillant",
+"Français, Anglais");
 
 INSERT INTO `instage`.`entreprise`
-(`Description`,
+(`IdEntreprise`,
+`Description`,
 `PersonneReference`,
-`Compte_IdCompte`,
-`Offre_IdOffre`,
-`Publicite_idPublicite`)
+`Compte_IdCompte`)
 VALUES
-("je suis une entreprise",
-"Pipin",
-2,
-1,
-1);
+(1,
+"Rio tinto",
+"Jack Sparrow",
+ 3);
 
-INSERT INTO `instage`.`etudiant`
-(`Nom`,
-`Prenom`,
-`NumeroDA`,
-`Compte_IdCompte`,
-`CV_IdCV`,
-`LettreMotivation_IdLettreMotivation`
-)
-VALUES
-("Smith",
-"John",
-1234567,
-3,
-1,
-1);
 
 INSERT INTO `instage`.`lettremotivation`
-(`Contenu`,
+(`IdLettreMotivation`,
+`Contenu`,
 `Titre`)
 VALUES
-("BLABLABLA Contenu ...",
-"Blablabla titre ...");
+(1,
+"Contenu etc ...",
+"Ma lettre");
 
 INSERT INTO `instage`.`occupation`
-(`DateDebut`,
+(`IdOccupation`,
+`DateDebut`,
 `DateFinl`,
-`Disponible`,
-`Offre_IdOffre`)
+`Disponible`)
 VALUES
-('2022-12-04',
-'2023-02-04',
-0,
-1);
+(1,
+'2022-01-01' ,
+'2023-01-01' ,
+0);
+INSERT INTO `instage`.`etudiant`
+(`IdEtudiant`,
+`Nom`,
+`Prenom`,
+`NumeroDA`,
+`LettreMotivation_IdLettreMotivation`,
+`CV_IdCV`,
+`Occupation_IdOccupation`,
+`Compte_IdCompte`)
+VALUES
+(1,
+"Bison",
+"Carl",
+12345,
+1,
+1,
+1,
+4);
+
+
 
 INSERT INTO `instage`.`offre`
-(`Poste`,
+(`IdOffre`,
+`Poste`,
 `Description`,
 `Remuneration`,
-`Entreprise_IdEntreprise`,
-`Entreprise_Compte_IdCompte`)
+`Entreprise_IdEntreprise`)
 VALUES
-("Stagiaire Programmeur",
-"blablabla description",
+(1,
+"Développeur Web",
+"Back-end",
 20.50,
 1);
 
-
-
-INSERT INTO `instage`.`professeur`
-(`Nom`,
-`Prenom`,
-`NumeroDa`,
-`Compte_IdCompte`,
+INSERT INTO `instage`.`postulation`
+(`Signature`,
+`Acceptation`,
+`Offre_IdOffre`,
 `Etudiant_IdEtudiant`)
 VALUES
-("Pattinson",
-"Paul",
-41343543,
-4,
+(0,
+1,
+1,
 1);
 
-INSERT INTO `instage`.`publicite`
-(
-`Contenu`,
-`ImgLink`)
+INSERT INTO `instage`.`professeur`
+(`IdProfesseur`,
+`Nom`,
+`Prenom`,
+`NumeroDa`,
+`Compte_IdCompte`)
 VALUES
-("contenu ....",
-"Pub.png");
+(1,
+"Sacquet",
+"Frodo",
+4321,
+2);
+
+INSERT INTO `instage`.`publicite`
+(`idPublicite`,
+`Contenu`,
+`ImgLink`,
+`Entreprise_IdEntreprise`)
+VALUES
+(1,
+"Contenu blabla bla",
+"pub.png",
+1);
