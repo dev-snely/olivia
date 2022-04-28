115df31a55dc655020871792e00342163cfde2f0
Snely Adam Lys <1826841@crosemont.qc.ca>
2022-04-27 20:54:41 -0400

    Html & CSS Tableau de bord minimale + Sidenav (interactivité)

--- Olivia/src/java/com/dao/etudiant/EtudiantDaoImpl.java
+++ Olivia/src/java/com/dao/etudiant/EtudiantDaoImpl.java
@@ -5,53 +5,268 @@
  */
 package com.dao.etudiant;
 
+import com.dao.compte.CompteDaoImpl;
+import com.model.entities.Compte;
 import com.model.entities.Etudiant;
+import com.services.singleton.ConnexionBD;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author gabri
  */
-public class EtudiantDaoImpl implements EtudiantDao{
+public class EtudiantDaoImpl implements EtudiantDao {
+
+    private static final String SQL_SELECT = "select * from etudiant";
+    private static final String SQL_SELECT_PAR_ID = "select * from etudiant where IdEtudiant = ?";
+    private static final String SQL_SELECT_PAR_NOM = "select * from admin where Nom = ?";
+    private static final String SQL_SELECT_PAR_PRENOM = "select * from admin where Prenom = ?";
+    private static final String SQL_INSET = "insert into admin(Nom,Prenom,Compte_IdCompte) value(?,?,?)";
+    private static final String SQL_UPDATE = "update admin set Nom =?,Prenom = ? where IdAdmin = ?";
+    private static final String SQL_DELETE = "delete from admin where IdAdmin = ?";
+    CompteDaoImpl daoCompte = new CompteDaoImpl();
 
     @Override
     public List<Etudiant> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Etudiant> listeEtudiants = null;
+
+        try {
+            //Initialise la requête préparée basée sur la connexion 
+            // la requête SQL passé en argument pour construire l'objet preparedStatement
+            PreparedStatement ps = ConnexionBD.getConnection().prepareStatement(SQL_SELECT);
+            //On execute la requête et on récupère les résultats dans la requête 
+            // dans ResultSet
+            ResultSet result = ps.executeQuery();
+
+            listeEtudiants = new ArrayList<>();
+
+            //// la méthode next() pour se déplacer sur l'enregistrement suivant
+            //on parcours ligne par ligne les résultas retournés
+            while (result.next()) {
+                //on enregistre les données dans un entities (bean, classe java)
+
+                Etudiant etudiant = new Etudiant();
+                etudiant.setId(result.getInt("IdAEtudiant"));
+                etudiant.setNom(result.getString("Nom"));
+                etudiant.setPrenom(result.getString("Prenom"));
+                etudiant.setNumeroDa(result.getInt("NumeroDa"));
+                etudiant.setCompte(daoCompte.findById(result.getInt("Compte_IdCompte")));
+
+                listeEtudiants.add(etudiant);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnexionBD.closeConnection();
+        return listeEtudiants;
     }
 
     @Override
     public Etudiant findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Etudiant etudiant = new Etudiant();
+        try {
+            //Initialise la requête préparée basée sur la connexion 
+            // la requête SQL passé en argument pour construire l'objet preparedStatement
+            PreparedStatement ps = ConnexionBD.getConnection().prepareStatement(SQL_SELECT_PAR_ID);
+            //On execute la requête et on récupère les résultats dans la requête 
+            // dans ResultSet
+            ps.setInt(1, id);
+            ResultSet result = ps.executeQuery();
+            while (result.next()) {
+                //// la méthode next() pour se déplacer sur l'enregistrement suivant
+                //on parcours ligne par ligne les résultas retournés
+                //on enregistre les données dans un entities (bean, classe java)
+                etudiant.setId(result.getInt("IdEtudiant"));
+                etudiant.setNom(result.getString("Nom"));
+                etudiant.setPrenom(result.getString("Prenom"));
+                etudiant.setCompte(daoCompte.findById(result.getInt("Compte_IdCompte")));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnexionBD.closeConnection();
+        return etudiant;
     }
 
     @Override
     public List<Etudiant> findByNom(String nom) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Etudiant> listeEtudiant = null;
+        try {
+            //Initialise la requête préparée basée sur la connexion 
+            // la requête SQL passé en argument pour construire l'objet preparedStatement
+            PreparedStatement ps = ConnexionBD.getConnection().prepareStatement(SQL_SELECT_PAR_NOM);
+            //On execute la requête et on récupère les résultats dans la requête 
+            // dans ResultSet
+            ps.setString(1, nom);
+            ResultSet result = ps.executeQuery();
+            listeEtudiant = new ArrayList<>();
+            //// la méthode next() pour se déplacer sur l'enregistrement suivant
+            //on parcours ligne par ligne les résultas retournés
+            while (result.next()) {
+                //on enregistre les données dans un entities (bean, classe java)
+                Etudiant etu = new Etudiant();
+                etu.setId(result.getInt("IdEtudiant"));
+                etu.setNom(result.getString("Nom"));
+                etu.setPrenom(result.getString("Prenom"));
+                etu.setCompte(daoCompte.findById(result.getInt("Compte_IdCompte")));
+
+                listeEtudiant.add(etu);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnexionBD.closeConnection();
+        return listeEtudiant;
     }
 
     @Override
     public List<Etudiant> findByPrenom(String prenom) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Etudiant> listeEtudiant = null;
+        try {
+            //Initialise la requête préparée basée sur la connexion 
+            // la requête SQL passé en argument pour construire l'objet preparedStatement
+            PreparedStatement ps = ConnexionBD.getConnection().prepareStatement(SQL_SELECT_PAR_NOM);
+            //On execute la requête et on récupère les résultats dans la requête 
+            // dans ResultSet
+            ps.setString(1, prenom);
+            ResultSet result = ps.executeQuery();
+            listeEtudiant = new ArrayList<>();
+            //// la méthode next() pour se déplacer sur l'enregistrement suivant
+            //on parcours ligne par ligne les résultas retournés
+            while (result.next()) {
+                //on enregistre les données dans un entities (bean, classe java)
+                Etudiant etu = new Etudiant();
+                etu.setId(result.getInt("IdEtudiant"));
+                etu.setNom(result.getString("Nom"));
+                etu.setPrenom(result.getString("Prenom"));
+                etu.setCompte(daoCompte.findById(result.getInt("Compte_IdCompte")));
+
+                listeEtudiant.add(etu);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnexionBD.closeConnection();
+        return listeEtudiant;
     }
 
     @Override
     public List<Etudiant> findByNumeroDA(int DA) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Etudiant> listeEtudiant = null;
+        try {
+            //Initialise la requête préparée basée sur la connexion 
+            // la requête SQL passé en argument pour construire l'objet preparedStatement
+            PreparedStatement ps = ConnexionBD.getConnection().prepareStatement(SQL_SELECT_PAR_NOM);
+            //On execute la requête et on récupère les résultats dans la requête 
+            // dans ResultSet
+            ps.setInt(1, DA);
+            ResultSet result = ps.executeQuery();
+            listeEtudiant = new ArrayList<>();
+            //// la méthode next() pour se déplacer sur l'enregistrement suivant
+            //on parcours ligne par ligne les résultas retournés
+            while (result.next()) {
+                //on enregistre les données dans un entities (bean, classe java)
+                Etudiant etu = new Etudiant();
+                etu.setId(result.getInt("IdEtudiant"));
+                etu.setNom(result.getString("Nom"));
+                etu.setPrenom(result.getString("Prenom"));
+                etu.setCompte(daoCompte.findById(result.getInt("Compte_IdCompte")));
+
+                listeEtudiant.add(etu);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnexionBD.closeConnection();
+        return listeEtudiant;
     }
 
     @Override
     public boolean create(Etudiant etu) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        Compte compte =etu.getCompte();
+        boolean retour = false;
+        int nbLigne = 0;
+        PreparedStatement ps;
+        try {
 
+            ps = ConnexionBD.getConnection().prepareStatement(SQL_INSET);
+            
+            ps.setString(1, etu.getNom());
+            ps.setString(2, etu.getPrenom());
+            ps.setInt(3, compte.getId());
+
+            nbLigne = ps.executeUpdate();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        if (nbLigne > 0) {
+            retour = true;
+        }
+        ConnexionBD.closeConnection();
+        return retour;
+    }
+    
     @Override
     public boolean delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean retour = false;
+        int nbLigne = 0;
+        PreparedStatement ps;
+        try {
+            ps = ConnexionBD.getConnection().prepareStatement(SQL_DELETE);
+
+            ps.setInt(1, id);
+
+
+            nbLigne = ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (nbLigne > 0) {
+            retour = true;
+        }
+        ConnexionBD.closeConnection();
+        return retour;
     }
 
     @Override
     public boolean update(Etudiant etu) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean retour = false;
+
+        int nbLigne = 0;
+        PreparedStatement ps;
+        try {
+            ps = ConnexionBD.getConnection().prepareStatement(SQL_UPDATE);
+
+            ps.setString(1, etu.getNom());
+            ps.setString(2, etu.getPrenom());
+            ps.setInt(3, etu.getId());
+
+
+            nbLigne = ps.executeUpdate();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            Logger.getLogger(CompteDaoImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        //System.out.println("nb ligne " + nbLigne);
+        if (nbLigne > 0) {
+            retour = true;
+        }
+        ConnexionBD.closeConnection();
+        return retour;
     }
-    
+
 }
--- Olivia/web/homePage.jsp
+++ Olivia/web/homePage.jsp
@@ -13,6 +13,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Home Page</title>
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
     </head>
     <body>
         <header class="alignerElementsHaut">
@@ -23,11 +25,11 @@
             </div>
 
             <div class="buttonsConnexionInscription">
-              
+
                 <a href="#" class="nomCompte">
-                    
+
                     Bonjour <%=session.getAttribute("prenom")%> <%=session.getAttribute("nom")%>  
-                
+
                 </a>
                 <a href="#"><button class="InscriptionButon">Se déconnecter</button> </a>
             </div>
@@ -38,7 +40,7 @@
             <ul>
                 <li><a class="navcoteText" href="#">Accueil</a></li>
                 <li><a class="navcoteText" href="#">Tableau de bord</a></li>
-                <li><a class="navcoteText" href="#">Recherche d'occupations</a></li>
+                <li><a class="navcoteText" href="#">Recherche</a></li>
                 <li><a class="navcoteText" href="#">Boite d'offres</a></li>
                 <li><a class="navcoteText" href="#">Curriculum Vitae</a></li>
                 <li><a class="navcoteText" href="#">Lettre motivation</a></li>
@@ -49,10 +51,49 @@
 
 
         <main class="contenuPrincipal">
-            <!-- Inserer la jsp ici-->
-
-
-
+            <!-- Transformer le code du main en jsp pour les trois type de compte-->
+            <h1 class="tbd-titre">TABLEAU DE BORD</h1><br>
+            <h2 class="tbd-sous-titre">Vos informations</h2>
+            <hr>
+            <table class="tbd-table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Courriel</th>
+                        <th>Lien vers Curriculum Vitae</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><%=session.getAttribute("nom")%></td>
+                        <td><%=session.getAttribute("prenom")%></td>
+                        <td>courriel@gmail.com</td>
+                        <td>lien vers CV</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <h2 class="tbd-sous-titre">Statut du stage</h2>
+            <hr>
+            <table class="tbd-table">
+                <thead>
+                    <tr>
+                        <th>Nom de l'entreprise</th>
+                        <th>Retenu par l'entreprise</th>
+                        <th>Date de début</th>
+                        <th>Date de fin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>CHUM</td>
+                        <td>Decision non prise</td>
+                        <td>2022-06-02</td>
+                        <td>2022-09-02</td>
+                    </tr>
+                </tbody>
+            </table>            
         </main>
 
     </body>
